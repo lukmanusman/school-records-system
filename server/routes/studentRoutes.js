bfbe23b6e22ce15a04d0bf45424dc9c90a4d3f56
Lukman Usman <lukman10usman@gmail.com>
2026-09-26 13:14:56 +0100

    Protect student profile updates with admin authorization

--- server/routes/studentRoutes.js
+++ server/routes/studentRoutes.js
@@ -1,14 +1,20 @@
 import express from "express";
+
 import {
   getStudents,
   getStudentById,
   updateStudent,
 } from "../controllers/studentController.js";
 
+import { protect } from "../middleware/authMiddleware.js";
+import { authorize } from "../middleware/roleMiddleware.js";
+
 const router = express.Router();
 
 router.get("/", getStudents);
+
 router.get("/:id", getStudentById);
-router.patch("/:id", updateStudent);
+
+router.patch("/:id", protect, authorize("ADMIN"), updateStudent);
 
 export default router;
