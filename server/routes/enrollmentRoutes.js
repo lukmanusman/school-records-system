fe273683851e98349e464e44184902b0ec3c2be8
Lukman Usman <lukman10usman@gmail.com>
2026-09-26 14:57:42 +0100

    Protect student enrollment with admin authorization

--- server/routes/enrollmentRoutes.js
+++ server/routes/enrollmentRoutes.js
@@ -1,8 +1,12 @@
 import express from "express";
+
 import { enrollStudent } from "../controllers/enrollmentController.js";
+
+import { protect } from "../middleware/authMiddleware.js";
+import { authorize } from "../middleware/roleMiddleware.js";
 
 const router = express.Router();
 
-router.post("/", enrollStudent);
+router.post("/", protect, authorize("ADMIN"), enrollStudent);
 
 export default router;
