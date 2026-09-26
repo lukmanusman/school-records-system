fd67fe7bb4921324f728e7ff9ffb5b4b51f4865b
Lukman Usman <lukman10usman@gmail.com>
2026-09-26 06:41:58 +0100

    Protect teacher registration with admin authorization

--- server/routes/teacherRoutes.js
+++ server/routes/teacherRoutes.js
@@ -1,14 +1,18 @@
 import express from "express";
+
 import {
   getTeachers,
   createTeacher,
   updateTeacher,
 } from "../controllers/teacherController.js";
 
+import { protect } from "../middleware/authMiddleware.js";
+import { authorize } from "../middleware/roleMiddleware.js";
+
 const router = express.Router();
 
 router.get("/", getTeachers);
-router.post("/", createTeacher);
+router.post("/", protect, authorize("ADMIN"), createTeacher);
 router.patch("/:id", updateTeacher);
 
 export default router;
